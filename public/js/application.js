2d9818d0682eb2040a28adbfcacf758e7fca87c6
Iliyan Ganev <ganeviliyan@gmail.com>
2017-12-13 00:41:14 -0800

    improve layout

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$("#MenuIcon").click(function(){
 		console.log("asdf")
 		$("#MainMenu").css("left","0px");
@@ -22,4 +23,24 @@ $(document).ready(function(){
 		}
 		setTimeout(originallayout, 600);
 	});
+
+
+
+
+	$("li").click(function(){
+		$("#MainMenu").css("-webkit-clip-path","polygon(0 0,0% 0,100% 100%,0% 100%)");
+		function hideMenu(){
+			$("#MainMenu").css("left","-300px");
+			$("#MenuIcon").animate({right:'50'},300);
+		}
+		setTimeout(hideMenu, 300);
+
+		function originallayout(){
+			$("#MainMenu").css("-webkit-clip-path","polygon(0 0,100% 0,0% 100%,0% 100%)");
+		}
+		setTimeout(originallayout, 600);
+	});
+
+
+
 });
